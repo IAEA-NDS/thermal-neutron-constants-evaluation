7ef997a337a6b0e2653f19aec44e5594a487d40a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-04-08 14:11:41 +0200

    move inference setup and inference step in two separate files

---
+++ tnc_axton_eval/tnc_inference_monolithic.py
@@ -0,0 +1,190 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+import pandas as pd
+import numpy as np
+import scipy as scp
+from utils import timeit
+import logging
+import sys
+
+logging.basicConfig(level=logging.WARNING, stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s')
+optim_prep_logger.setLevel(logging.INFO)
+
+from prior_prep import (
+    tuple_combis_mic as tuple_combis,
+    reac_map_mic as reac_map,
+    assign_startvals,
+    startvals_map,
+)
+from propagate_prep import (
+    prepare_propagate,
+    prepare_jacobian,
+)
+import optim_prep
+from optim_prep import (
+    prepare_chisquare,
+    prepare_chisquare_gradient,
+    prepare_chisquare_and_gradient,
+    prepare_chisquare_hessian,
+    logger as optim_prep_logger,
+)
+from expdata import (
+    all_exp_dt as exp_dt
+)
+from gmapy.tf_uq.inference import (
+    determine_MAP_estimate,
+    generate_MCMC_chain,
+)
+from gmapy.mcmc_inference import compute_effective_sample_size
+
+
+# NOTE: directly starting with UNC_MODE='scale_by_propvals'
+#       would not give nice results (formally converged yes, but weird numbers).
+#       First, good values should be found with UNC_MODE='scale_by_expvals',
+#       and the result as starting values in the fit with UNC_MODE='scale_by_propvals'
+
+optim_prep.UNC_MODE = 'scale_by_expvals'
+
+seed = np.random.randint(0, 1000, 1).item()
+np.random.seed(seed)
+startvals = np.random.uniform(1, 2, len(tuple_combis))
+
+# select experimental datasets
+failure_reacs = []
+selected_exp_idx = []
+for i in range(len(exp_dt)):
+    exp_dt_row = exp_dt.iloc[i:i+1].reset_index()
+    propfun = prepare_propagate(reac_map, exp_dt_row)
+    print('dataset: ' + str(exp_dt_row.iloc[0]['No']))
+    reac = exp_dt_row.iloc[0]['MeasureFunc']
+    print(reac)
+    try:
+        propfun(startvals)
+        selected_exp_idx.append(i)
+    except:
+        failure_reacs.append(reac)
+
+# ['FLEM', 'CAP(34)', 'FFH(39,39)', 'FFH(39,39)/FFH(34,35)', 'FFH(39,39)/FFH(33,33)', 'FFH(39,39)/FFH(33,35)', 'F1BIG', 'FH1(39,39)', 'FH1(34,35)', 'FH1(34,35)', 'FH1(39,39)', 'FH1(39,39)/FH1(34,35)',
+#  'FH1(39,39) / FH1(34,35)', 'F1CAB', 'F2CAB', 'F3CAB', 'F4CAB', 'F5CAB', 'CA(40)', 'CA(42)', 'GC116(39)', 'GC116(40)', 'GC116(41)', 'GC116(42)', 'GA116(39)', 'GA116(41)']
+
+
+red_exp_dt = exp_dt.loc[selected_exp_idx].reset_index(drop=True)
+
+propagate = prepare_propagate(reac_map, red_exp_dt)
+jacobian = prepare_jacobian(propagate)
+assign_startvals(startvals, startvals_map, reac_map)
+startvals_tf = tf.constant(np.sqrt(np.abs(startvals)), dtype=tf.float64)
+
+expvals = red_exp_dt['InputValue'].to_numpy()
+
+# preapre the relative covariance matrix
+# relcov_linop = tf.linalg.LinearOperatorDiag(
+#     np.square(red_exp_dt['Uncertainty'] / 100.0), is_positive_definite=True
+# )
+
+ags_index = np.loadtxt('tnc_cov_data/thermalcst.mic')
+cov_info = np.loadtxt('tnc_cov_data/ags.mic')
+assert np.all(ags_index[:,0] == cov_info[:,0])
+
+no_gilles = ags_index[:,0].astype(int).tolist()
+no_georg = red_exp_dt['No'].tolist()
+
+stat_uncs = cov_info[:,2]
+partial_uncs = cov_info[:,3:]
+
+rel_covmat = sum([x.reshape(-1,1) @ x.reshape(1,-1) for x in partial_uncs.T])
+rel_covmat += np.diag(np.square(stat_uncs))
+
+full_covmat = np.diag(np.square(red_exp_dt['Uncertainty'] / 100))
+idcs = [no_georg.index(n) for n in no_gilles if n in no_georg]
+sel = [n in no_georg for n in no_gilles]
+full_covmat[np.ix_(idcs, idcs)] = rel_covmat[np.ix_(sel, sel)]
+
+relcov_linop = tf.linalg.LinearOperatorFullMatrix(full_covmat, is_positive_definite=True, is_square=True)
+
+# prepare the fit quantities
+trafo = tf.square
+chisquare = prepare_chisquare(propagate, expvals, relcov_linop, trafo=trafo)
+chisquare_and_gradient = prepare_chisquare_and_gradient(chisquare)
+chisquare_hessian = prepare_chisquare_hessian(chisquare)
+
+func_and_grad_tf = tf.function(chisquare_and_gradient)
+func_hessian_tf = tf.function(chisquare_hessian)
+
+# do a MAP
+optres = determine_MAP_estimate(startvals_tf, func_and_grad_tf, func_hessian_tf, ret_optres=True)
+opt_params = (trafo(optres.position)).numpy()
+
+# do MCMC
+log_prob = lambda x: (-chisquare(x))
+# chain, _ = generate_MCMC_chain(startvals_tf, log_prob, chisquare_hessian, num_leapfrog_steps=3, step_size=0.1, num_results=20000)
+opt_params_mcmc = np.mean(trafo(chain).numpy(), axis=0)
+
+
+if 'res_list' not in globals():
+    res_list = []
+
+post_df = pd.DataFrame({
+    'NAME': [f'{x} {y}' for x, y in tuple_combis],
+    'POST': opt_params,
+    'POST_MCMC': opt_params_mcmc,
+    'SEED': seed,
+})
+
+
+# method 1
+chisquare_notrafo = prepare_chisquare(propagate, expvals, relcov_linop)
+chisquare_hessian_notrafo = prepare_chisquare_hessian(chisquare_notrafo)
+invpostcov = chisquare_hessian_notrafo(tf.constant(opt_params))
+postcov = np.linalg.inv(invpostcov)
+postuncs = np.sqrt(postcov.diagonal())
+post_df['RELUNC1'] = postuncs / post_df['POST'] * 100
+
+# method 2
+propfun = prepare_propagate(reac_map, red_exp_dt)
+propvals = propfun(opt_params).numpy()
+jacfun = prepare_jacobian(propfun)
+J = jacfun(tf.constant(opt_params))
+expcov = relcov_linop.to_dense().numpy() * (propvals.reshape(-1, 1) @ propvals.reshape(1,-1))
+inv_expcov = np.linalg.inv(expcov)
+postcov = np.linalg.inv(J.numpy().T @ inv_expcov @ J.numpy())
+postuncs = np.sqrt(postcov.diagonal())
+post_df['RELUNC2'] = postuncs / post_df['POST'] * 100
+
+# method 3
+postuncs_mcmc = np.std(trafo(chain).numpy(), axis=0)
+post_df['RELUNC_MCMC'] = postuncs_mcmc / post_df['POST_MCMC'] * 100
+
+
+import matplotlib.pyplot as plt
+curchain = trafo(chain[:,14]).numpy()
+scp.stats.skew(curchain)
+scp.stats.kurtosis(curchain)
+compute_effective_sample_size(curchain)
+plt.hist(curchain, bins=50)
+plt.show()
+
+fis_df = post_df[post_df['NAME'].str.startswith('FIS')].reset_index(drop=True)
+abs_df = post_df[post_df['NAME'].str.startswith('ABS')].reset_index(drop=True)
+cap_df = fis_df.copy()
+cap_df['NAME'] = cap_df['NAME'].str.replace('FIS', 'CA')
+cap_df['POST'] = abs_df['POST'] - fis_df['POST']
+cap_df['POST_MCMC'] = abs_df['POST_MCMC'] - fis_df['POST_MCMC']
+
+red_post_df = post_df[~post_df['NAME'].str.startswith('ABS') & ~post_df['NAME'].str.startswith('HLF')]
+red_post_df = pd.concat([red_post_df, cap_df], axis=0, ignore_index=True)
+red_post_df
+
+
+# comparison with experiments
+# red_exp_dt['PRED'] = propagate(opt_params)
+# red_exp_dt['RES'] = (red_exp_dt['PRED'] - red_exp_dt['InputValue']) / red_exp_dt['InputValue'] / (red_exp_dt['Uncertainty']/100)
+# red_exp_dt
+
+idcs = post_df[post_df.NAME.str.startswith('FIS')].index
+for idx in idcs:
+    curchain = trafo(chain[:,idx]).numpy()
+    curskew = scp.stats.skew(curchain)
+    post_df.loc[idx, 'SKEW'] = curskew
+    curkurt = scp.stats.kurtosis(curchain)
+    post_df.loc[idx, 'KURT'] = curkurt
---
+++ tnc_axton_eval/tnc_inference_prep.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+import pandas as pd
+import numpy as np
+import scipy as scp
+from utils import timeit
+import logging
+import sys
+
+from prior_prep import (
+    tuple_combis_mic as tuple_combis,
+    reac_map_mic as reac_map,
+    assign_startvals,
+    startvals_map,
+)
+from propagate_prep import (
+    prepare_propagate,
+    prepare_jacobian,
+)
+import optim_prep
+from optim_prep import (
+    prepare_chisquare,
+    prepare_chisquare_gradient,
+    prepare_chisquare_and_gradient,
+    prepare_chisquare_hessian,
+    logger as optim_prep_logger,
+)
+from expdata import (
+    all_exp_dt as exp_dt
+)
+
+logging.basicConfig(level=logging.WARNING, stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s')
+optim_prep_logger.setLevel(logging.INFO)
+
+# NOTE: directly starting with UNC_MODE='scale_by_propvals'
+#       would not give nice results (formally converged yes, but weird numbers).
+#       First, good values should be found with UNC_MODE='scale_by_expvals',
+#       and the result as starting values in the fit with UNC_MODE='scale_by_propvals'
+
+optim_prep.UNC_MODE = 'scale_by_expvals'
+
+seed = np.random.randint(0, 1000, 1).item()
+np.random.seed(seed)
+startvals = np.random.uniform(1, 2, len(tuple_combis))
+
+# select experimental datasets
+failure_reacs = []
+selected_exp_idx = []
+for i in range(len(exp_dt)):
+    exp_dt_row = exp_dt.iloc[i:i+1].reset_index()
+    propfun = prepare_propagate(reac_map, exp_dt_row)
+    print('dataset: ' + str(exp_dt_row.iloc[0]['No']))
+    reac = exp_dt_row.iloc[0]['MeasureFunc']
+    print(reac)
+    try:
+        propfun(startvals)
+        selected_exp_idx.append(i)
+    except:
+        failure_reacs.append(reac)
+
+# ['FLEM', 'CAP(34)', 'FFH(39,39)', 'FFH(39,39)/FFH(34,35)', 'FFH(39,39)/FFH(33,33)', 'FFH(39,39)/FFH(33,35)', 'F1BIG', 'FH1(39,39)', 'FH1(34,35)', 'FH1(34,35)', 'FH1(39,39)', 'FH1(39,39)/FH1(34,35)',
+#  'FH1(39,39) / FH1(34,35)', 'F1CAB', 'F2CAB', 'F3CAB', 'F4CAB', 'F5CAB', 'CA(40)', 'CA(42)', 'GC116(39)', 'GC116(40)', 'GC116(41)', 'GC116(42)', 'GA116(39)', 'GA116(41)']
+
+
+red_exp_dt = exp_dt.loc[selected_exp_idx].reset_index(drop=True)
+
+propagate = prepare_propagate(reac_map, red_exp_dt)
+jacobian = prepare_jacobian(propagate)
+assign_startvals(startvals, startvals_map, reac_map)
+startvals_tf = tf.constant(np.sqrt(np.abs(startvals)), dtype=tf.float64)
+
+expvals = red_exp_dt['InputValue'].to_numpy()
+
+# preapre the relative covariance matrix
+# relcov_linop = tf.linalg.LinearOperatorDiag(
+#     np.square(red_exp_dt['Uncertainty'] / 100.0), is_positive_definite=True
+# )
+
+ags_index = np.loadtxt('tnc_cov_data/thermalcst.mic')
+cov_info = np.loadtxt('tnc_cov_data/ags.mic')
+assert np.all(ags_index[:,0] == cov_info[:,0])
+
+no_gilles = ags_index[:,0].astype(int).tolist()
+no_georg = red_exp_dt['No'].tolist()
+
+stat_uncs = cov_info[:,2]
+partial_uncs = cov_info[:,3:]
+
+rel_covmat = sum([x.reshape(-1,1) @ x.reshape(1,-1) for x in partial_uncs.T])
+rel_covmat += np.diag(np.square(stat_uncs))
+
+full_covmat = np.diag(np.square(red_exp_dt['Uncertainty'] / 100))
+idcs = [no_georg.index(n) for n in no_gilles if n in no_georg]
+sel = [n in no_georg for n in no_gilles]
+full_covmat[np.ix_(idcs, idcs)] = rel_covmat[np.ix_(sel, sel)]
+
+relcov_linop = tf.linalg.LinearOperatorFullMatrix(full_covmat, is_positive_definite=True, is_square=True)
+
+# prepare the fit quantities
+trafo = tf.square
+chisquare = prepare_chisquare(propagate, expvals, relcov_linop, trafo=trafo)
+chisquare_and_gradient = prepare_chisquare_and_gradient(chisquare)
+chisquare_hessian = prepare_chisquare_hessian(chisquare)
+
+func_and_grad_tf = tf.function(chisquare_and_gradient)
+func_hessian_tf = tf.function(chisquare_hessian)
